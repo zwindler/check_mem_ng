13e515cda7fe185946c062fdf1de67c3bf8af003
zwindler <dt.germain@gmail.com>
2016-07-18 19:41:31 +0200

    Update check_mem_ng.sh
    Wrong licence

--- check_mem_ng.sh
+++ check_mem_ng.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 ################################################################################
-#This file is a part of zwindler-monitoring-plugins repository
-#Copyright (C) 2015 zwindler
-#
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License
-#along with this program. If not, see {http://www.gnu.org/licenses/}.
-################################################################################
 #This plugin is loosely inspired by check_mem v1.1 from Lukasz Gogolin
 #https://exchange.nagios.org/directory/Plugins/System-Metrics/Memory/check_mem-2Esh/details
 #I did a code cleanup and added a check of "free" version, because since
